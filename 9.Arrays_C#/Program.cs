65235af8781e70ae75bd77dd3e756950cf54b106
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-12 09:48:38 +0200

    Add project files.

---
+++ 9.Arrays_C#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Arrays_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration of arrays
+            int[] luckyNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+            Console.WriteLine(luckyNumbers.Length);
+            Console.WriteLine(luckyNumbers[0]);
+            Console.WriteLine("The max is " + luckyNumbers.Max());
+
+            string[] friends = new string[5];
+            friends[0] = "Charlie";
+            friends[1] = "Thato";
+            friends[2] = Console.ReadLine();
+            friends[3] = "Tetious";
+
+            Console.WriteLine(friends[0]);
+            Console.WriteLine(friends[1]);
+            Console.WriteLine(friends[2]);
+
+            Console.ReadLine();
+
+        }
+    }
+}
